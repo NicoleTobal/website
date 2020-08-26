b79db65f554e2b608af4eb4e78c053a9bba1e5b1
Nicole Tobal <nickytobal@gmail.com>
2020-08-26 18:28:26 -0300

    Animations added

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
 FROM nginx
 
+# Label is added to identify image version
+LABEL version="1.0"
+
+# Nginx config file is copied
 COPY nginx.conf /etc/nginx/nginx.conf
 
+# Since it is plain html no CMD command is needed, just to copy in nginx directory
 COPY . /usr/share/nginx/html
--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
+# Removes existing container, uses force in order to avoid stopping container
 docker rm -f /my-website
+# Builds new image
 docker build -t my-website-image .
+# Cleans old images
+docker image prune -f
+# Runs container from image
 docker run --name my-website -p 8081:80 -d my-website-image
